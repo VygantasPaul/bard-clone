f48b0189a1153879f2d7b9ae17cad61db096b86e
vygantaspaul <wygantas.pauliukonis@gmail.com>
2024-01-16 17:23:12 +0200

    latest heading

--- components/Content/Latest/Latest.tsx
+++ components/Content/Latest/Latest.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-
-const Latest = ({ heading }) => {
+type HeadingType = {
+  heading: string;
+};
+const Latest: React.FC<HeadingType> = ({ heading }) => {
   return (
     <div className="text-xs w-full pb-3 pl-2">
       <p className="leading-4 font-semibold">{heading}</p>
