ef0cd4f0e8525d71373ab6e2aa0bcfd105f403ba
vygantaspaul <wygantas.pauliukonis@gmail.com>
2024-01-16 17:27:35 +0200

    location

--- components/Content/Location/Location.tsx
+++ components/Content/Location/Location.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Inter } from "next/font/google";
 const interBold = Inter({ subsets: ["latin"], weight: "500" });
-const Location = ({ location_name, update_location, by_location }) => {
+type LocationType = {
+  location_name: string;
+  update_location: string;
+  by_location: string;
+};
+const Location: React.FC<LocationType> = ({
+  location_name,
+  update_location,
+  by_location,
+}) => {
   return (
     <div className="pl-2 md:pl-3 lg:pl-4">
       <ul>
