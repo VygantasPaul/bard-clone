104d7808d2ff82cbcbc5ba0ae5b3684c2e5c13f5
vygantaspaul <wygantas.pauliukonis@gmail.com>
2024-01-17 12:59:19 +0200

    privacy

--- components/Content/Privacy/Privacy.tsx
+++ components/Content/Privacy/Privacy.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-
-const Privacy = ({ text }) => {
+type PrivacyType = {
+  text: string;
+};
+const Privacy: React.FC<PrivacyType> = ({ text }) => {
   return (
     <div className="p-2 text-center">
       <p className="text-xs">{text}</p>
